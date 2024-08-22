3c60e0d2effe39429c4d434a92eb8af4b18fc640
José Victor <josevictornascimento2016@gmail.com>
2024-08-21 21:54:39 -0300

    feat: date input modal

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,56 @@
-import { Image, Text, View } from "react-native";
-import { MapPin, Calendar as IconCalendar } from "lucide-react-native";
-import { Input } from "@/components/input";
+import { useState } from "react";
+
+import { Image, Keyboard, Text, View } from "react-native";
+import {
+  MapPin,
+  Calendar as IconCalendar,
+  Settings2,
+  UserRoundPlus,
+} from "lucide-react-native";
+import { DateData } from "react-native-calendars";
+import dayjs from "dayjs";
+
 import { colors } from "@/styles/colors";
+import { calendarUtils, DatesSelected } from "@/utils/calendarUtils";
+
+import { Input } from "@/components/input";
+import { Button } from "@/components/button";
+import { Modal } from "@/components/modal";
+import { Calendar } from "@/components/calendar";
+
+enum StepForm {
+  TRIP_DETAILS = 1,
+  ADD_EMAIL = 2,
+}
+
+enum MODAL {
+  NONE = 0,
+  CALENDAR = 1,
+  GUESTS = 2,
+}
 
 export default function Index() {
+  const [stepForm, setStepForm] = useState(StepForm.TRIP_DETAILS);
+  const [selectedDates, setSelectedDates] = useState({} as DatesSelected);
+
+  const [showModal, setShowModal] = useState(MODAL.NONE);
+
+  function handleNextStepFrom() {
+    if (stepForm === StepForm.TRIP_DETAILS) {
+      return setStepForm(StepForm.ADD_EMAIL);
+    }
+  }
+
+  function handleSelectDate(selectedDay: DateData) {
+    const dates = calendarUtils.orderStartsAtAndEndsAt({
+      startsAt: selectedDates.startsAt,
+      endsAt: selectedDates.endsAt,
+      selectedDay,
+    });
+
+    setSelectedDates(dates);
+  }
+
   return (
     <View className="flex-1 items-center justify-center px-5">
       <Image
@@ -11,6 +58,8 @@ export default function Index() {
         className="h-8"
         resizeMode="contain"
       />
+
+      <Image source={require("@/assets/bg.png")} className="absolute" />
 
       <Text className="text-zinc-400 font-regular text-center text-lg mt-3">
         Convide seus amigos e planeje sua{"\n"}
@@ -20,16 +69,81 @@ export default function Index() {
       <View className="w-full bg-zinc-900 p-4 rounded-xl my-8 border border-zinc-800">
         <Input>
           <MapPin color={colors.zinc[400]} size={20} />
-          <Input.Field placeholder="Para onde?" />
+          <Input.Field
+            placeholder="Para onde?"
+            editable={stepForm === StepForm.TRIP_DETAILS}
+          />
         </Input>
 
         <Input>
           <MapPin color={colors.zinc[400]} size={20} />
-          <Input.Field placeholder="Para onde?" />
+          <Input.Field
+            placeholder="Quando?"
+            editable={stepForm === StepForm.TRIP_DETAILS}
+            onFocus={() => Keyboard.dismiss()}
+            showSoftInputOnFocus={false}
+            onPress={() =>
+              stepForm === StepForm.TRIP_DETAILS && setShowModal(MODAL.CALENDAR)
+            }
+            value={selectedDates.formatDatesInText}
+          />
         </Input>
 
-        <View className="border-b py-3 border-zinc-800"></View>
+        {stepForm === StepForm.ADD_EMAIL && (
+          <>
+            <View className="border-b py-3 border-zinc-800">
+              <Button
+                variant="secondary"
+                onPress={() => setStepForm(StepForm.TRIP_DETAILS)}
+              >
+                <Button.Title>Alterar local/data</Button.Title>
+                <Settings2 color={colors.zinc[200]} size={20} />
+              </Button>
+            </View>
+
+            <Input>
+              <UserRoundPlus color={colors.zinc[400]} size={20} />
+              <Input.Field placeholder="Quem estará na viagem?" />
+            </Input>
+          </>
+        )}
+
+        <Button onPress={handleNextStepFrom}>
+          <Button.Title>
+            {stepForm === StepForm.TRIP_DETAILS
+              ? "Continuar"
+              : "Confirmar Viagem"}
+          </Button.Title>
+          <Settings2 color={colors.lime[950]} size={20} />
+        </Button>
       </View>
+
+      <Text className="text-zinc-500 font-regular text-center text-base">
+        Ao planejar sua viagem pela plann.er você automaticamente concorda com
+        nossos{" "}
+        <Text className="text-zinc-300 underline">
+          termos de uso e políticas de privacidade.
+        </Text>
+      </Text>
+
+      <Modal
+        title="Selecione datas"
+        subtitle="Selecione a data de ida e volta da viagem"
+        visible={showModal === MODAL.CALENDAR}
+        onClose={() => setShowModal(MODAL.NONE)}
+      >
+        <View className="gap-4 mt-4">
+          <Calendar
+            minDate={dayjs().toISOString()}
+            onDayPress={handleSelectDate}
+            markedDates={selectedDates.dates}
+          />
+
+          <Button onPress={() => setShowModal(MODAL.NONE)}>
+            <Button.Title>Confirmar</Button.Title>
+          </Button>
+        </View>
+      </Modal>
     </View>
   );
 }
---
+++ src/server/trip-server.ts
@@ -0,0 +1,45 @@
+import { api } from "./api";
+
+export type TripDetails = {
+  id: string;
+  destination: string;
+  starts_at: string;
+  ends_at: string;
+  is_confirmed: boolean;
+};
+
+type TripCreate = Omit<TripDetails, "id" | "is_confirmed"> & {
+  emails_to_invite: string[];
+};
+
+async function getById(id: string) {
+  try {
+    const { data } = await api.get<{ trip: TripDetails }>(`/trips/${id}`);
+
+    return data.trip;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function create({
+  destination,
+  starts_at,
+  ends_at,
+  emails_to_invite,
+}: TripCreate) {
+  try {
+    const {} = await api.post<{ tripId: string }>("/trips", {
+      destination,
+      starts_at,
+      ends_at,
+      emails_to_invite,
+      owner_name: "José Victor",
+      owner_email: "josevictor@email.com",
+    });
+  } catch (error) {
+    throw error;
+  }
+}
+
+export const tripServer = { getById, create };
